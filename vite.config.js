d05db2ecbbae84e7aa902f354a0290ab140c8c44
Farstar09 <71098116+Farstar09@users.noreply.github.com>
2025-11-20 12:11:53 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // IMPORTANT for custom domains
+  base: '/', // correct for custom domain
 })
