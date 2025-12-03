73144aee02bdb859a6b04912b16b73453fa884af
Farstar09 <71098116+Farstar09@users.noreply.github.com>
2025-12-03 12:12:06 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TeamPage from "./pages/TeamPage";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/team/:teamName" element={<TeamPage />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
